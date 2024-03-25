9be619bc0fa9af777b8e6e3f56754f7aad18e324
adnan-as <boic4448@gmail.com>
2024-03-25 11:23:11 +0530

    my first commit

---
+++ to_do_list.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add_task():
+    tesk = entry.get()
+    if tesk:
+        listbox.insert(tk.END,tesk)
+        entry.delete(0,tk.END)
+    else:
+        messagebox.showwarning("please enter a tesk")
+    
+def delete_task():
+    try:
+        selected_index = listbox.curselection()
+        listbox.delete(selected_index)
+    except:
+        messagebox.showwarning("please select a tesk to delete")
+
+#create a window
+win = tk.Tk()
+win.title("to do list")
+
+#create and add widget
+
+entry = tk.Entry(win, width=30)
+entry.pack()
+
+add_button = tk.Button(win, text="add tesk",command=add_task)
+add_button.pack()
+
+delete_button = tk.Button(win, text="delete task", command=delete_task)
+delete_button.pack()
+
+listbox = tk.Listbox(win, selectmode=tk.SINGLE, width=40,height=10)
+listbox.pack()
+
+win.mainloop()
